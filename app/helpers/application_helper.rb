048b650fd2484f650a7a5ceeeb59f5c2582ad9ea
Hana <18086006+khana87@users.noreply.github.com>
2019-06-06 23:14:01 +0000

    Make links and the tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      # byebug
+      base_title
+    else
+      # byebug
+      page_title + " | " + base_title
+    end
+  end
 end
